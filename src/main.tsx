c239c12a3742db1771cb05285a03f4c9a82c2c46
Edems <57250017+Edems-DEV@users.noreply.github.com>
2024-11-29 17:54:16 +0100

    feat: [broken] mock service

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,24 @@ import App from "./App.tsx";
 import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import './i18n.ts'
+import "./i18n.ts";
 
 const client = new QueryClient();
 
-createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={client}>
-    <StrictMode>
-      <Suspense fallback="loading...">
-        <App />
-      </Suspense>
-    </StrictMode>
-  </QueryClientProvider>
-);
+async function deferRender() {
+  if (process.env.NODE_ENV != "development") return;
+  const { worker } = await import("./mocks/browser.js");
+  worker.start();
+}
+
+deferRender().then(() => {
+  createRoot(document.getElementById("root")!).render(
+    <QueryClientProvider client={client}>
+      <StrictMode>
+        <Suspense fallback="loading...">
+          <App />
+        </Suspense>
+      </StrictMode>
+    </QueryClientProvider>
+  );
+});
