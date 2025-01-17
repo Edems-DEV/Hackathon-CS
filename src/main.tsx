cb416c449d8beb9ce48d4402ff5bee8bf86d17fa
Edems <57250017+Edems-DEV@users.noreply.github.com>
2025-01-17 15:17:34 +0100

    fix: revert to before mock
    manual revert to: a8f74097d5eaacd21b1c2eb3546d90c58ce849fa
    ( fix: graph is filtered )

--- src/main.tsx
+++ src/main.tsx
@@ -3,24 +3,16 @@ import App from "./App.tsx";
 import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import "./i18n.ts";
+import './i18n.ts'
 
 const client = new QueryClient();
 
-async function deferRender() {
-  if (process.env.NODE_ENV != "development") return;
-  const { worker } = await import("./mocks/browser.js");
-  worker.start();
-}
-
-deferRender().then(() => {
-  createRoot(document.getElementById("root")!).render(
-    <QueryClientProvider client={client}>
-      <StrictMode>
-        <Suspense fallback="loading...">
-          <App />
-        </Suspense>
-      </StrictMode>
-    </QueryClientProvider>
-  );
-});
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={client}>
+    <StrictMode>
+      <Suspense fallback="loading...">
+        <App />
+      </Suspense>
+    </StrictMode>
+  </QueryClientProvider>
+);
